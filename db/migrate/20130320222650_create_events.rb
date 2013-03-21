3ea1d11a5945d000961c06c2b6f5cf01e16a68b4
ccandy <ccunandy@yahoo.com>
2013-03-21 14:27:09 -0700

    fix the link of referrallog link

---
+++ db/migrate/20130320222650_create_events.rb
@@ -0,0 +1,20 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.references :program
+      t.integer :program_id
+      t.references :eventtype
+      t.integer :eventtype_id
+      t.references :facility
+      t.integer :facility_id
+      t.integer :duration
+      t.string :date
+
+      t.timestamps
+    end
+    add_index :events, :program_id
+    add_index :events, :eventtype_id
+    add_index :events, :facility_id
+  end
+end
