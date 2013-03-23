a0ebaf19bd626e07d6877c2663275cf936d8fc6e
fraser <fraseraddison@gmail.com>
2013-03-23 03:27:26 -0700

    added pg for production db. created seed file for default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Staff.create(email: 'a@b.c', password: '12345678')
